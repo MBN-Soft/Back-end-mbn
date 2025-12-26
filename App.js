4ff56ef4e5c7f75c7552779cf2d30c5838d993a1
MoustafaHp <mostafahp44@gmail.com>
2025-12-26 16:25:43 +0200

    Update App.js upload Images

--- App.js
+++ App.js
@@ -1,38 +1,38 @@
 // src/app.js
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./Config/db");
 
 const authRoutes = require("./Routes/authRoutes");
 const contactRoutes = require("./Routes/contactRoutes");
 const adminRoutes = require("./Routes/adminRoutes");
-const ProjectRoutes = require("./Routes/ProjectRoutes");
+const projectRoutes = require("./Routes/ProjectRoutes");
 const articleRoutes = require("./Routes/articleRoutes");
-
-
 
 const app = express();
 
-// Middlewares
+// ================== Middlewares ==================
 app.use(cors());
 app.use(express.json());
 
-// Connect DB
+// ================== Static uploads ==================
+// 👇 مهم جدًا عشان الصور تفتح من المتصفح
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// ================== Connect DB ==================
 connectDB();
 
-// Routes
+// ================== Routes ==================
 app.use("/api/auth", authRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api", ProjectRoutes);
+app.use("/api", projectRoutes);
 app.use("/api", articleRoutes);
 
-
-
+// ================== Test route ==================
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-
 
 module.exports = app;
