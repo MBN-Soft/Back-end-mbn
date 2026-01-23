4c14e47d8b49ad6822e64c5cc827dd5d677447f7
MoustafaHp <mostafahp44@gmail.com>
2026-01-23 14:39:41 +0200

    Update validateObjectId.js

--- Midderlware/validateObjectId.js
+++ Midderlware/validateObjectId.js
@@ -1,10 +1,28 @@
-// src/middleware/validateObjectId.js
-const mongoose = require("mongoose");
+// // src/middleware/validateObjectId.js
+// const mongoose = require("mongoose");
 
+// const validateObjectId = (req, res, next) => {
+//   const { id } = req.params;
+
+//   if (!mongoose.Types.ObjectId.isValid(id)) {
+//     return res.status(400).json({
+//       success: false,
+//       message: "Invalid ID format",
+//     });
+//   }
+
+//   next();
+// };
+
+// module.exports = validateObjectId;
+// src/middleware/validateUUID.js
 const validateObjectId = (req, res, next) => {
   const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
+  const uuidRegex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+  if (!uuidRegex.test(id)) {
     return res.status(400).json({
       success: false,
       message: "Invalid ID format",
